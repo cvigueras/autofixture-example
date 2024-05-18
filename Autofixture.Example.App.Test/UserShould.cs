d161aa00fccbc6eaeaa78c3ab87bd03aeb82b781
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-18 16:43:13 +0200

    chore: init project

---
+++ Autofixture.Example.App.Test/UserShould.cs
@@ -0,0 +1,16 @@
+namespace Autofixture.Example.App.Test
+{
+    public class UserShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
